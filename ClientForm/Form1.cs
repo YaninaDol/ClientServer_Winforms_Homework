00b54cf1a235b29aec6e6b402448d2d827fe496c
Yana Doll <Dolz_op79@student.itstep.org>
2022-07-01 14:09:47 +0400

    Add project files.

---
+++ ClientForm/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        const int PORT = 8008;
+        const string IP_ADDR = "127.0.0.1";
+        IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP_ADDR), PORT);
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public Form1()
+        {
+            InitializeComponent();
+            this.Closing += Form1_Closing;
+            try
+            {
+                socket.Connect(iPEnd);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+
+        }
+
+        private void Form1_Closing(object sender, CancelEventArgs e)
+        {
+
+            MessageBox.Show("Are you shure?");
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (socket.Connected)
+            {
+                string str = "text";
+                byte[] dataname = Encoding.Unicode.GetBytes(str);
+                socket.Send(dataname);
+                string msg = this.textBox1.Text;
+                byte[] data = Encoding.Unicode.GetBytes(msg);
+                socket.Send(data);
+
+            }
+            else
+            {
+                MessageBox.Show("Socket not connected");
+            }
+
+            this.textBox1.Text = string.Empty;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.label1.Text = dialog.FileName;
+
+                }
+            }
+         
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string str = this.label1.Text;
+            string type = str.Substring(str.LastIndexOf('.'));
+            if (socket.Connected)
+            {
+                if (type.Equals(".txt"))
+                {
+                    string filetype = ".txt";
+
+                    byte[] datafiletype = Encoding.Unicode.GetBytes(filetype);
+                    socket.Send(datafiletype);
+                    string filepath = this.label1.Text;
+
+                    FileInfo file = new FileInfo(filepath);
+                    string size = Convert.ToString(file.Length);
+                    byte[] datasize = Encoding.Unicode.GetBytes(size);
+                    socket.Send(datasize);
+
+                    byte[] datafile = File.ReadAllBytes(filepath);
+                    socket.Send(datafile);
+                }
+                if (type.Equals(".png"))
+                {
+                    string filetype = ".png";
+                    byte[] datafiletype = Encoding.Unicode.GetBytes(filetype);
+                    socket.Send(datafiletype);
+
+                    string filepath = this.label1.Text;
+
+                    FileInfo file = new FileInfo(filepath);
+                    string size = Convert.ToString(file.Length);
+                    byte[] datasize = Encoding.Unicode.GetBytes(size);
+                    socket.Send(datasize);
+                    
+
+
+                    byte[] datafile = File.ReadAllBytes(filepath);
+                    socket.Send(datafile);
+                }
+                if (type.Equals(".jpg"))
+                {
+                    string filetype = ".jpg";
+                    byte[] datafiletype = Encoding.Unicode.GetBytes(filetype);
+                    socket.Send(datafiletype);
+
+                    string filepath = this.label1.Text;
+
+                    FileInfo file = new FileInfo(filepath);
+                    string size = Convert.ToString(file.Length);
+                    byte[] datasize = Encoding.Unicode.GetBytes(size);
+                    socket.Send(datasize);
+
+
+
+                    byte[] datafile = File.ReadAllBytes(filepath);
+                    socket.Send(datafile);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Socket not connected");
+            }
+
+            this.label1.Text = string.Empty;
+        }
+    }
+}
---
+++ ClientServer_Winforms/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer_Winforms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PORT = 8008;
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            DirectoryInfo di = Directory.CreateDirectory(@"C:\Users\1\OneDrive\Рабочий стол\Servak");
+            Console.WriteLine("SERVER START");
+            int id = 1;
+
+            try
+            {
+                socket.Bind(iPEnd);
+                socket.Listen(10);
+
+                Socket clientSocket = socket.Accept();
+                while (clientSocket.Connected)
+                {
+                    
+                    Console.WriteLine("SERVER CATCH");
+                    int byteCount = 0;
+                    byte[] buffer = new byte[256];
+                    StringBuilder stringBuilder = new StringBuilder();
+                    do
+                    {
+                        byteCount = clientSocket.Receive(buffer);
+                        stringBuilder.Append(Encoding.Unicode.GetString(buffer, 0, byteCount));
+                    } while (clientSocket.Available > 0);
+                    string rez = stringBuilder.ToString();
+                    if (rez.Equals("text"))
+                    {
+                        Console.WriteLine("SERVER CATCH TEXT");
+                        int byteCount2 = 0;
+                        byte[] buffer2 = new byte[256];
+                        StringBuilder stringBuilder2 = new StringBuilder();
+                        do
+                        {
+                            byteCount2 = clientSocket.Receive(buffer2);
+                            stringBuilder2.Append(Encoding.Unicode.GetString(buffer2, 0, byteCount2));
+                        } while (clientSocket.Available > 0);
+
+                        string rez2 = stringBuilder2.ToString();
+                        string filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}MESAGGES.txt";
+                        File.AppendAllText(di.FullName + filename, rez2 + "\n");
+                    }
+                    if (rez.Equals(".txt"))
+                    {
+                        Console.WriteLine("SERVER CATCH FILE TXT");
+                        int byteCount2 = 0;
+                        byte[] buffer2 = new byte[256];
+                        StringBuilder sizeBuilder = new StringBuilder();
+                        do
+                        {
+                            byteCount2 = clientSocket.Receive(buffer2);
+                            sizeBuilder.Append(Encoding.Unicode.GetString(buffer2, 0, byteCount2));
+
+                        } while (clientSocket.Available > 0);
+
+                        long size = Convert.ToUInt32(sizeBuilder.ToString());
+
+                        int byteCount3 = 0;
+                        byte[] buffer3 = new byte[size];
+                        do
+                        {
+                            byteCount3 = clientSocket.Receive(buffer3);
+
+                        } while (clientSocket.Available > 0)
+                        ;
+                        string filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}copyTXT.txt";
+                        if (File.Exists(di.FullName + filename))
+                        {
+                            filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}copyTXT{id}.txt";
+                            File.WriteAllBytes(di.FullName + filename, buffer3);
+                            id++;
+                        }
+                        else
+
+                        { File.WriteAllBytes(di.FullName + filename, buffer3); }
+                    }
+                    if (rez.Equals(".png"))
+                    {
+                        Console.WriteLine("SERVER CATCH FILE PNG");
+                        int byteCount2 = 0;
+                        byte[] buffer2 = new byte[256];
+                        StringBuilder sizeBuilder = new StringBuilder();
+                        do
+                        {
+                            byteCount2 = clientSocket.Receive(buffer2);
+                            sizeBuilder.Append(Encoding.Unicode.GetString(buffer2, 0, byteCount2));
+
+                        } while (clientSocket.Available > 0);
+                        long size = Convert.ToUInt32(sizeBuilder.ToString());
+
+                        int byteCount3 = 0;
+                        byte[] buffer3 = new byte[size];
+
+                        do
+                        {
+                            byteCount3 = clientSocket.Receive(buffer3);
+
+                        } while (clientSocket.Available > 0);
+                        string filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}CopyPNG.png";
+                        if (File.Exists(di.FullName + filename))
+                        {
+                            filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}CopyPNG{id}.png";
+                            File.WriteAllBytes(di.FullName + filename, buffer3);
+                            id++;
+                        }
+                        else
+
+                        { File.WriteAllBytes(di.FullName + filename, buffer3); }
+
+                    }
+                    if (rez.Equals(".jpg"))
+                    {
+                        Console.WriteLine("SERVER CATCH FILE JPG");
+                        int byteCount2 = 0;
+                        byte[] buffer2 = new byte[256];
+                        StringBuilder sizeBuilder = new StringBuilder();
+                        do
+                        {
+                            byteCount2 = clientSocket.Receive(buffer2);
+                            sizeBuilder.Append(Encoding.Unicode.GetString(buffer2, 0, byteCount2));
+
+                        } while (clientSocket.Available > 0);
+                        long size = Convert.ToUInt32(sizeBuilder.ToString());
+
+                        int byteCount3 = 0;
+                        byte[] buffer3 = new byte[size];
+
+                        do
+                        {
+                            byteCount3 = clientSocket.Receive(buffer3);
+
+                        } while (clientSocket.Available > 0);
+
+                        string filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}CopyJPG.jpg";
+                        if (File.Exists(di.FullName + filename))
+                        {
+                             filename = $"\\{DateTime.Now.ToShortDateString()}{iPEnd.Address.ToString()}CopyJPG{id}.jpg";
+                            File.WriteAllBytes(di.FullName + filename, buffer3);
+                            id++;
+                        }
+                        else
+
+                        { File.WriteAllBytes(di.FullName + filename, buffer3); }
+
+                    }
+
+                }
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("SERVER END");
+            Console.ReadKey();
+        }
+    }
+}
